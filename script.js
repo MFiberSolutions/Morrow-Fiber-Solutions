61e2b9ccb759e460597279e5c7af8dd80fcb7e2b
MFiberSolutions <morrowfibersolutions@gmail.com>
2025-05-15 00:28:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('nav a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', smoothScroll);
-    });
-
-    function smoothScroll(event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({
-            behavior: 'smooth'
+    document.querySelectorAll('nav a, .cta').forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
         });
-    }
+    });
 });
